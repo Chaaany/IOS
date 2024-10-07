6dc01d9429bbf1faf4e026cdf40a2866a073243d
Chaaany <ben125@lguplus.co.kr>
2024-10-07 22:10:45 +0900

    add UIKIT storyboard

--- MyUIKit/MyUIKit/ViewController.swift
+++ MyUIKit/MyUIKit/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func didMyButtonTapped(_ sender: Any) {
+        myLabel.text = "Hello UIKit"
+    }
+    
 
 }
 
