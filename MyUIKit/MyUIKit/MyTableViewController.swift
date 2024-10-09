c7d92b71f8ac16c63946e2667e52c8777a3a34c5
Chaaany <ben125@lguplus.co.kr>
2024-10-09 22:51:10 +0900

    add delegate sample

--- MyUIKit/MyUIKit/MyTableViewController.swift
+++ MyUIKit/MyUIKit/MyTableViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class MyTableViewController: UIViewController  {
 
     @IBOutlet weak var myTableView: UITableView!
@@ -22,17 +23,19 @@ class MyTableViewController: UIViewController  {
     }
 }
 
-extension MyTableViewController: UITableViewDelegate, UITableViewDataSource {
+extension MyTableViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cellData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         cell.textLabel?.text = cellData[indexPath.row]
         return cell
     }
-
-    
 }
--- MyUIKit/MyUIKit/MyViewController.swift
+++ MyUIKit/MyUIKit/MyViewController.swift
@@ -7,21 +7,38 @@
 
 import UIKit
 
+protocol AdminDelegate {
+    func doTask()
+}
+
 class MyViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var helloLabel: UILabel!
+    var admin: Admin?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        admin = Admin(delegate: self)
     }
     @IBAction func didTapButton(_ sender: Any) {
         if let name = nameTextField.text {
             helloLabel.text = "Hello \(name)!"
         }
+        admin?.delegate.doTask()
         
         print("Hello Ben!")
     }
 }
+
+extension MyViewController: AdminDelegate {
+    func doTask() {
+        print("저 지금 일 잘하고 있습니다!")
+    }
+}
+
+struct Admin {
+    var delegate: AdminDelegate
+}
+
