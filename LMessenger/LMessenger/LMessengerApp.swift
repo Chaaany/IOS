187a34728735afef04c2791b1a5d46d772b836f2
Chaaany <ben125@lguplus.co.kr>
2024-10-12 23:06:35 +0900

    init LMessenger project

---
+++ LMessenger/LMessenger/LMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessengerApp.swift
+//  LMessenger
+//
+//  Created by Ben on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LMessenger/LMessengerTests/LMessengerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LMessengerTests.swift
+//  LMessengerTests
+//
+//  Created by Ben on 10/12/24.
+//
+
+import Testing
+@testable import LMessenger
+
+struct LMessengerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
